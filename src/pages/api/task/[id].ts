46843e7637849aefac01b90c8a44743d72cb5256
schutz-luca <lucaschutzenhofer@hotmail.com>
2023-12-14 22:29:21 -0300

    api changes

--- src/pages/api/task/[id].ts
+++ src/pages/api/task/[id].ts
@@ -1,24 +1,43 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { getSession } from 'next-auth/react';
+import { getServerSession } from 'next-auth';
 import prisma from "@/lib/prisma";
+import { authOptions } from '../auth/[...nextauth]';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  const taskId = req.query.id;
+  try {
+    const taskId = req.query.id;
+    const method = req.method;
 
-  const session = await getSession({ req })
+    const session = await getServerSession(req, res, authOptions);
+    if (!session)
+      res.status(401).send({ message: 'Unauthorized' })
 
-  if (req.method === "DELETE") {
-    if (session) {
-      const post = await prisma.task.delete({
+
+    if (method === 'DELETE') {
+      // DELETE
+      const task = await prisma.task.delete({
         where: { id: Number(taskId) },
       });
-      res.json(post);
-    } else {
-      res.status(401).send({ message: 'Unauthorized' })
-    }
-  } else {
-    throw new Error(
-      `The HTTP ${req.method} method is not supported at this route.`
-    );
+      res.json(task);
+    } else
+
+      if (method === 'PUT') {
+        // PUT
+        const body = req.body;
+
+        const task = await prisma.task.update({
+          data: body,
+          where: { id: Number(taskId) },
+        });
+        res.json(task);
+      }
+
+      else {
+        throw { message: `The HTTP ${req.method} method is not supported at this route` }
+      }
+  }
+  catch (error: any) {
+    console.error(error)
+    res.status(500).send({ message: error.message })
   }
 }
--- src/pages/api/task/index.ts
+++ src/pages/api/task/index.ts
@@ -4,20 +4,43 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '../auth/[...nextauth]';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  const { title, description } = req.body;
+  try {
+    const method = req.method;
 
-  const session = await getServerSession(req, res, authOptions);
+    const session = await getServerSession(req, res, authOptions);
+    if (!session?.user?.email) {
+      res.status(401).send({ message: 'Unauthorized' })
+      return;
+    }
 
-  if (session?.user?.email) {
-    const result = await prisma.task.create({
-      data: {
-        title,
-        description,
-        author: { connect: { email: session?.user?.email } },
-      },
-    });
-    res.json(result);
-  } else {
-    res.status(401).send({ message: 'Unauthorized' })
+    if (method === 'POST') {
+      const { title, description } = req.body;
+      const result = await prisma.task.create({
+        data: {
+          title,
+          description,
+          author: { connect: { email: session?.user?.email } },
+        },
+      });
+      res.json(result);
+    } else
+
+      if (method === 'GET') {
+        const tasks = await prisma.task.findMany({
+          where: {
+            author: { email: session.user.email },
+          },
+          include: {
+            author: {
+              select: { name: true },
+            },
+          },
+        })
+        res.json(tasks);
+      }
+  }
+  catch (error: any) {
+    console.error(error)
+    res.status(500).send({ message: error.message })
   }
 }
