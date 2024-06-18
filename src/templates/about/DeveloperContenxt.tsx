6ea2318f7fc334bd19baf8e8880b18be4b0d4fe1
schutz-luca <lucaschutzenhofer@hotmail.com>
2024-06-18 16:52:52 -0300

    reorder dev skills

--- src/templates/about/DeveloperContenxt.tsx
+++ src/templates/about/DeveloperContenxt.tsx
@@ -13,6 +13,9 @@ export const DeveloperContent = () => (
             <h4>Specialist</h4>
             <div className='flex'>
                 <Bubble text='React.js' />
+                <Bubble text='Context API' />
+                <Bubble text='CSS Skills' />
+                <Bubble text='JQuery' />
                 <Bubble text='Node.js' color='purple' />
                 <Bubble text='Express' color='purple' />
                 <Bubble text='Git' color='orange' />
@@ -25,12 +28,12 @@ export const DeveloperContent = () => (
                 <Bubble text='Next.js' />
                 <Bubble text='Framer Motion' />
                 <Bubble text='Redux' />
-                <Bubble text='Context API' />
                 <Bubble text='Nest.js' color='purple' />
                 <Bubble text='TypeOrm' color='purple' />
                 <Bubble text='PostgreSQL' color='purple' />
                 <Bubble text='AWS' color='orange' />
                 <Bubble text='Azure' color='orange' />
+                <Bubble text='DevOps' color='orange' />
             </div>
             <div className='divisor' />
             <h4>Acquainted</h4>
@@ -38,7 +41,6 @@ export const DeveloperContent = () => (
                 <Bubble text='React Native' />
                 <Bubble text='Angular' />
                 <Bubble text='Vue.js' />
-                <Bubble text='DevOps' color='orange' />
                 <Bubble text='Docker' color='orange' />
                 <Bubble text='IA Prompts' color='orange' />
                 <Bubble text='Elasticsearch' color='orange' />
