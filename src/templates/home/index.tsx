77e803d8184a5844542e07053457abe1bb56c625
schutz-luca <lucaschutzenhofer@hotmail.com>
2025-07-30 01:44:39 -0300

    refine wording in home section for clarity

--- src/templates/home/index.tsx
+++ src/templates/home/index.tsx
@@ -17,7 +17,7 @@ export const Home = () => (
                     <h2><b>Front-end Expert</b> {'//'} <b>Full-stack Engineer</b> {'//'} <b>Tech Lead</b></h2>
                     <p>
                         <b>Software Architect</b> focused in <b>Creative Solutions</b>. Postgraduated in Software Architecture & Solutions, skilled across the full digital product lifecycle, from
-                        requirements to front-end and back-end development, CI/CD in the cloud, version control, and production release management.
+                        requirements to front-end and back-end development, CI/CD using cloud services, version control, and worldwide production release management.
                     </p>
                     <br />
                     <p>
