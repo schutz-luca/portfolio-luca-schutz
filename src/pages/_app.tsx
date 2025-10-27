bca0fd3fea893d0e53631aa3bbd01d492323cfb2
schutz-luca <lucaschutzenhofer@hotmail.com>
2025-10-27 17:31:41 -0300

    fix: standardize import quotes for consistency

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from '@vercel/analytics/next';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeManagerProvider } from '@/src/context/app-manager';
